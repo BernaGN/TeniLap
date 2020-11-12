c0922a41ca2377a8b81e03fa308d27a43834becf
BernaGN <31524330+BernaGN@users.noreply.github.com>
2020-11-11 22:14:28 -0600

    Poner en el diseño algunas tablas

--- resources/views/admin/layouts/navegacion.blade.php
+++ resources/views/admin/layouts/navegacion.blade.php
@@ -1,34 +1,5 @@
-<div class="sidebar-shortcuts" id="sidebar-shortcuts">
-    <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
-        <button class="btn btn-success">
-            <i class="ace-icon fa fa-signal"></i>
-        </button>
 
-        <button class="btn btn-info">
-            <i class="ace-icon fa fa-pencil"></i>
-        </button>
-
-        <button class="btn btn-warning">
-            <i class="ace-icon fa fa-users"></i>
-        </button>
-
-        <button class="btn btn-danger">
-            <i class="ace-icon fa fa-cogs"></i>
-        </button>
-    </div>
-
-    <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
-        <span class="btn btn-success"></span>
-
-        <span class="btn btn-info"></span>
-
-        <span class="btn btn-warning"></span>
-
-        <span class="btn btn-danger"></span>
-    </div>
-    </div><!-- /.sidebar-shortcuts -->
-
-    <ul class="nav nav-list">
+<ul class="nav nav-list">
     <li class="">
         <a href="{{ route('inicio.index') }}">
             <i class="menu-icon fa fa-tachometer"></i>
@@ -103,5 +74,4 @@
             <b class="arrow"></b>
         </a>
     </li>
-
 </ul><!-- /.nav-list -->
