75110be8f6b5e23c06ef2dfdc984bcf5dcb45925
BernaGN <31524330+BernaGN@users.noreply.github.com>
2020-11-24 19:40:31 -0600

    Agregar modelos a los controladores

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Dispositivo;
+use DB;
+use Illuminate\Support\Facades\Redirect;
 
 class PagoController extends Controller
 {
