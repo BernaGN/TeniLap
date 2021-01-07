b1eda2ac49b0314c7246ebeb1162131b1bd53bff
BernaGN <31524330+BernaGN@users.noreply.github.com>
2021-01-07 15:25:41 -0600

    Buscar

--- resources/views/usuario/layouts/plantilla.blade.php
+++ resources/views/usuario/layouts/plantilla.blade.php
@@ -1,70 +1,51 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>Home - Tecni App</title>
-        <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i')}}">
-        <link rel="stylesheet" href="{{asset('assets/fonts/font-awesome.min.css')}}">
-        <link rel="stylesheet" href="{{asset('assets/css/best-carousel-slide.css')}}">
-        <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css')}}">
-        <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css')}}">
-        <link rel="stylesheet" href="{{asset('assets/css/smoothproducts.css')}}">
-    </head>
 
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Tecni App</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/best-carousel-slide.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/smoothproducts.css') }}">
+</head>
 
-        @include('usuario.layouts.navegacion-usuario')
+<body>
 
-        @yield('seccion')
+    @include('usuario.layouts.navegacion-usuario')
 
-        <footer class="page-footer dark">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <h5>Get started</h5>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Sign up</a></li>
-                            <li><a href="#">Downloads</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-3">
-                        <h5>About us</h5>
-                        <ul>
-                            <li><a href="#">Company Information</a></li>
-                            <li><a href="#">Contact us</a></li>
-                            <li><a href="#">Reviews</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-3">
-                        <h5>Support</h5>
-                        <ul>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Help desk</a></li>
-                            <li><a href="#">Forums</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-3">
-                        <h5>Legal</h5>
-                        <ul>
-                            <li><a href="#">Terms of Service</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                        </ul>
-                    </div>
+    @yield('seccion')
+
+    <footer class="page-footer dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
                 </div>
             </div>
-            <div class="footer-copyright">
-                <p>© 2020 Copyright Text</p>
-            </div>
-        </footer>
-        <script src="{{asset('assets/js/jquery.min.js')}}"></script>
-        <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
-        <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js')}}"></script>
-        <script src="{{asset('assets/js/smoothproducts.min.js')}}"></script>
-        <script src="{{asset('assets/js/theme.js')}}"></script>
-    </body>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2020 Copyright Text</p>
+        </div>
+    </footer>
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js') }}">
+    </script>
+    <script src="{{ asset('assets/js/smoothproducts.min.js') }}"></script>
+    <script src="{{ asset('assets/js/theme.js') }}"></script>
+</body>
 
 </html>
