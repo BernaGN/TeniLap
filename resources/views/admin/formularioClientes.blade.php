e5262829c63c58d817fdc2f78b079d6b27c4a44c
BernaGN <31524330+BernaGN@users.noreply.github.com>
2021-01-07 13:27:28 -0600

    clientes

--- resources/views/admin/formularioClientes.blade.php
+++ resources/views/admin/formularioClientes.blade.php
@@ -1,19 +1,20 @@
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="nombre">Nombre</label>
     <div class="col-md-9">
-        <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre del cliente" required pattern="^[a-zA-Z_áéíóúñ\s]{0,30}$">
+        <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre del cliente" required
+            pattern="^[a-zA-Z_áéíóúñ\s]{0,30}$">
     </div>
 </div>
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="descripcion">Email</label>
     <div class="col-md-9">
-    <input type="text" name="email" id="email" class="form-control" placeholder="Ingrese Email">
+        <input type="text" name="email" id="email" class="form-control" placeholder="Ingrese Email">
     </div>
 </div>
 <div class="form-group row">
     <label class="col-md-3 form-control-label" for="descripcion">Telefono</label>
     <div class="col-md-9">
-    <input type="text" name="telefono" id="telefono" class="form-control" placeholder="Ingrese su Telefono">
+        <input type="text" name="telefono" id="telefono" class="form-control" placeholder="Ingrese su Telefono">
     </div>
 </div>
 <div class="form-group row">
@@ -31,10 +32,11 @@
     <div class="col-md-9">
         <select name="localidad" id="localidad" class="form-control">
             <option value=""></option>
-            @isset($localidades)
-            @foreach($localidades as $localidad)
-            <option value="{{$localidad->id}}" {{$cliente->localidad == $localidad->nombre ? 'selected' : ''}}>{{$localidad->nombre}}</option>
-            @endforeach
+            @isset($localidades, $cliente)
+                @foreach ($localidades as $localidad)
+                    <option value="{{ $localidad->id }}" {{ $cliente->localidad == $localidad->nombre ? 'selected' : '' }}>
+                        {{ $localidad->nombre }}</option>
+                @endforeach
             @endisset
         </select>
     </div>
