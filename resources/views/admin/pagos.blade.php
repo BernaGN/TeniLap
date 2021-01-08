8f8a386de3f010f3fdb1cff528c85ded27975786
BernaGN <31524330+BernaGN@users.noreply.github.com>
2021-01-07 23:13:45 -0600

    Mapa

--- resources/views/admin/pagos.blade.php
+++ resources/views/admin/pagos.blade.php
@@ -1,26 +1,37 @@
 @extends('admin.layouts.plantilla')
 
 @section('seccion')
+
     <body class="no-skin">
         @include('admin.layouts.menu')
         <div class="main-container" id="main-container">
             <script type="text/javascript">
-                try{ace.settings.check('main-container' , 'fixed')}catch(e){}
+                try {
+                    ace.settings.check('main-container', 'fixed')
+                } catch (e) {}
+
             </script>
 
             <div id="sidebar" class="sidebar responsive">
                 <script type="text/javascript">
-                    try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    try {
+                        ace.settings.check('sidebar', 'fixed')
+                    } catch (e) {}
+
                 </script>
 
                 @include('admin.layouts.navegacion')
 
                 <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
-                    <i class="ace-icon fa fa-angle-double-left" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+                    <i class="ace-icon fa fa-angle-double-left" data-icon1="ace-icon fa fa-angle-double-left"
+                        data-icon2="ace-icon fa fa-angle-double-right"></i>
                 </div>
 
                 <script type="text/javascript">
-                    try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    try {
+                        ace.settings.check('sidebar', 'collapsed')
+                    } catch (e) {}
+
                 </script>
             </div>
 
@@ -45,12 +56,12 @@
                                     </thead>
 
                                     <tbody>
-                                        @foreach($dispositivos as $dispositivo)
-                                        <tr>
-                                            <td>{{$dispositivo->tipo." ".$dispositivo->marca}}</td>
-                                            <td>{{$dispositivo->total}}</td>
-                                            <td>{{$dispositivo->cliente}}</td>
-                                        </tr>
+                                        @foreach ($dispositivos as $dispositivo)
+                                            <tr>
+                                                <td>{{ $dispositivo->tipo . ' ' . $dispositivo->marca }}</td>
+                                                <td>{{ $dispositivo->anticipo }}</td>
+                                                <td>{{ $dispositivo->cliente }}</td>
+                                            </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
@@ -66,6 +77,6 @@
                 <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
             </a>
         </div><!-- /.main-container -->
-    @include('admin.layouts.funcionesIndex')
+        @include('admin.layouts.funcionesIndex')
     </body>
 @endsection
