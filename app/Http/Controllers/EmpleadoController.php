d04189a9a6d77deb799ac874785508206e2eaa9d
BernaGN <31524330+BernaGN@users.noreply.github.com>
2021-01-09 13:50:51 -0600

    Clientes

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use DB;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Hash;
 
 class EmpleadoController extends Controller
 {
